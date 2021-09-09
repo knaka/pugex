18aa7d9011c2471038b8a0bfbff29167edc28665
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2021-09-09 22:32:08 +0900

    Connect pug and posthtml

--- src/index.js
+++ src/index.js
@@ -1,27 +1,69 @@
 #!/usr/bin/env node
 "use strict";
+
+const pug = require('pug');
 const posthtml = require("posthtml");
 const sass = require("sass");
-const html = `
-<html lang="en">
-  <style type="text/sass">
-    table#foo {
-      td, th {
-        text-align: right;
-      }
-    }
-  </style>
-</html>
-`;
+const beautify = require('posthtml-beautify')
+const fs = require('fs');
+const path = require("path");
+
 (async () => {
-    const result = await posthtml([x({})])
-        .process(html);
-    console.log("d:" + result.html);
+  const inFile = process.argv[2]
+  const outFile = path.join(path.dirname(inFile), path.basename(inFile, path.extname(inFile)) + ".html")
+  const htmlInput = pug.renderFile(inFile)
+  const result = await posthtml([
+    sassPlugin(),
+    psvPlugin(),
+    beautify({rules: {
+      indent: 2,
+      blankLines: false,
+    }})
+  ])
+    .process(htmlInput.toString());
+  fs.writeFileSync(outFile, result.html)
 })();
-function x(options) {
+
+function psvPlugin() {
+  return (tree) => {
+    tree.match({tag: 'div', attrs: {"class": "psvtable"}}, (node) => {
+      let tabhead = []
+      let tabbody = []
+      node.content[0].split("\n").forEach((line) => {
+        if (line.trim() === "") {
+          return
+        }
+        const elems = line.split("|");
+        (tabhead.length == 0? tabhead: tabbody).push({
+          tag: "tr",
+          content: elems.map((elem) => {
+            return {tag: "td", content: elem.trim()}
+          })
+        })
+      })
+      return {
+        tag: 'table',
+        attrs: node.attrs,
+        content: [
+          {
+            tag: 'thead',
+            content: tabhead
+          },
+          {
+            tag: "tbody",
+            content: tabbody
+          }
+        ]
+      };
+    });
+    return tree;
+  };
+}
+
+function sassPlugin() {
     return (tree) => {
         tree.match({ tag: 'style', attrs: { "type": "text/sass" } }, (node) => {
-            const result = sass.renderSync({ data: node.content[0] });
+            const result = sass.renderSync({data: node.content[0]});
             const css = result.css.toString();
             return {
                 tag: 'style',
@@ -32,4 +74,3 @@ function x(options) {
         return tree;
     };
 }
-//# sourceMappingURL=index.js.map
